85dd4c8ae2fbd6dc7b021c2b708b250ce02d41a5
Yadhu <yadhukrishnanas@gmail.com>
2023-07-01 11:53:19 +0530

    added 200

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.use("/", (req,res) => {
+app.get("/", (req,res) => {
     res.sendStatus(404)
 });
 
+app.get("/found", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.listen(8000, function () {
   console.log("Server running at 8000");
