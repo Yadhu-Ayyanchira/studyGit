1c4da3d7ce0620c3affa58d369b7d39f31cf843f
Yadhu <yadhukrishnanas@gmail.com>
2023-07-01 11:50:14 +0530

    added 404

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hey i am one');
+const express = require('express')
+const app = express()
+
+app.use("/", (req,res) => {
+    res.sendStatus(404)
+});
+
+
+app.listen(8000, function () {
+  console.log("Server running at 8000");
+});
